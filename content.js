f7a68083a32a21a42202ba167c666b9366dfcded
ian-chuang <iantc104@gmail.com>
2020-11-16 21:08:28 -0800

    made grades that link to grade page of that specific course

--- content.js
+++ content.js
@@ -1,10 +1,15 @@
-var header = document.querySelector('#my_courses_table thead .course-list-nickname-column');
-header.textContent = 'Grades';
+var column = Array.from(document.querySelectorAll('#my_courses_table .course-list-nickname-column'));
+column[0].textContent = 'Grades';
+
+var links = Array.from(document.querySelectorAll('#my_courses_table .course-list-course-title-column a')).map(function(a){
+	return a.getAttribute('href');
+});
 
 var grades = ['B- (80.7%)', 'A (94.3)%', 'B+ (87.6%)', 'C- (72.3%)', 'A+ (100%)'];
 
-var column = Array.from(document.querySelectorAll('#my_courses_table tbody .course-list-nickname-column'));
-
-for(var i = 0; i < column.length; i++){
-	column[i].textContent = grades[i];
+for(var i = 1; i < column.length; i++){
+	const a = document.createElement('a');
+	a.setAttribute('href', links[i-1] + '/grades');
+	a.textContent = grades[i];
+	column[i].appendChild(a);
 }
