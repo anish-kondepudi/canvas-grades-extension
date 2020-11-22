2593064382402403ee8c548931c08ffcffcf556b
ian-chuang <iantc104@gmail.com>
2020-11-22 13:29:21 -0800

    might be buggy lol but added color code to each course name to mirror what it was like in courses page

--- EditDashboard.js
+++ EditDashboard.js
@@ -10,6 +10,21 @@ fetch('/api/v1/courses?include[]=total_scores&per_page=100&enrollment_state=acti
     .then(res => res.json())
     .then(function(grades_data){
 
+      var id_colors = {};
+
+      var colors = Array.from(document.querySelectorAll('.ic-DashboardCard__header_hero')).map(function(div){
+      	return div.style.backgroundColor;
+      });
+
+      var ids = Array.from(document.querySelectorAll('.ic-DashboardCard__link')).map(function(a){
+      	return a.getAttribute('href').substring(9);
+      });
+
+      for (var i = 0; i < colors.length; i++){
+      	id_colors[ids[i]] = colors[i];
+      }
+
+
       //Array.from(document.querySelectorAll('.ic-DashboardCard')).forEach(function(tile){
       	//tile.parentNode.removeChild(tile);
       //});
@@ -28,14 +43,16 @@ fetch('/api/v1/courses?include[]=total_scores&per_page=100&enrollment_state=acti
       for (var j = 0; j < 2; j++) {
             // Create a table "tab" element
             var new_tab = document.createElement('th');
+            new_tab.setAttribute('scope', 'col');
             new_tab.setAttribute('class', 'course-list-course-title-column course-list-no-left-border');
-            new_tab.style.width = '50%';
             // Create a text element and depending on the column
             // set its text to "Course" or "Grade"
             if(j == 0){
               new_tab.textContent = 'Course';
+              new_tab.style.width = '50%';
             }else{
               new_tab.textContent = 'Grades';
+              new_tab.style.width = '50%';
             }
 
             // Append the text object to the tab object, and
@@ -60,18 +77,33 @@ fetch('/api/v1/courses?include[]=total_scores&per_page=100&enrollment_state=acti
             var new_tab = document.createElement('td');
             new_tab.setAttribute('class', 'course-list-course-title-column course-list-no-left-border');
 
-            new_tab.style.width = '50%';
+            
             // Create a text element and depending on the column
             // set its text to the course name or the users grade
             var link = document.createElement('a');
             if(j == 0){
+              const span = document.createElement('span');
+              span.setAttribute('aria-hidden', 'true');
+              span.setAttribute('class', 'course-color-block');
+              span.style.backgroundColor = id_colors[grades_data[i].id];
+              span.style.color = id_colors[grades_data[i].id];
+              span.style.height = '.65rem';
+              span.style.width = '.65rem';
+              span.style.float = 'left';
+              span.style.marginRight = '.5rem';
+              span.style.marginTop = '.2rem';
+              new_tab.append(span);
+
+
               link.setAttribute('href', '/courses/' + grades_data[i].id);
               link.textContent = grades_data[i].name;
+              new_tab.style.width = '50%';
             }else{
               var course_score = grades_data[i].enrollments[grades_data[i].enrollments.length - 1].computed_current_score;
               var course_grade = grades_data[i].enrollments[grades_data[i].enrollments.length - 1].computed_current_grade;
               link.setAttribute('href', '/courses/' + grades_data[i].id + '/grades');
               link.textContent = course_grade == null ? "N/A" : course_grade + " (" + course_score + "%)";
+              new_tab.style.width = '50%';
             }
 
             // Append the text object to the tab object, and
@@ -96,4 +128,7 @@ fetch('/api/v1/courses?include[]=total_scores&per_page=100&enrollment_state=acti
 
       // insert the html element into the html of the dashboard page
       document.getElementById("DashboardCard_Container").prepend(new_element);
+})
+.catch((error) => {
+  console.error('Error:', error);
 });
